5a56b05281bf1e8c0884ceca29b2bbba4bee800a
Rudi Farkas <rudi.farkas@gmail.com>
2022-05-10 20:16:59 +0200

    add info-links

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import {MyElement} from './src/my-element';
 import {UserDataElement} from './src/user-data-element';
 import {NamesElement} from './src/names/names-element';
+import {InfoLinks} from './src/info-links';
